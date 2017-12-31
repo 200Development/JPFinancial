d0e5a97b7c665d3d75f28ebaf1d6f1ed1fa6e662
Ryan Parsons <ryan@stackadvisors.com>
2017-12-30 23:39:25 -0700

    adds TransactionDTO and uses Transaction class as model of db table

--- JPFinancial/Models/Transaction.cs
+++ JPFinancial/Models/Transaction.cs
@@ -1,26 +1,21 @@
-﻿
-using JPFinancial.Models.Enumerations;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace JPFinancial.Models
 {
     public class Transaction
     {
+        [Key]
+        public int Id { get; set; }
         public DateTime Date { get; set; }
         public string Payee { get; set; }
         public string Memo { get; set; }
+        public string Type { get; set; }
+        public string Category { get; set; }
+        public string TransferTo { get; set; }
+        public string TransferFrom { get; set; }
         public decimal? Spend { get; set; }
         public decimal? Receive { get; set; }
         public decimal? Amount { get; set; }
-        public decimal? Balance { get; set; }
-        public TransactionTypesEnum TypesEnum { get; set; }
-        [JsonProperty(PropertyName = "Category")]
-        public IncomeCategoriesEnum IncomeCategoryEnum { get; set; }
-        public List<Account> DebitAccounts { get; set; }
-        public List<Account> CreditAccounts { get; set; }
-        public Account SelectedDebitAccount { get; set; }
-        public Account SelectedCreditAccount { get; set; }
     }
 }
