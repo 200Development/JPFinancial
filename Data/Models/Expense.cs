88f271d0befd706dc95777c6694d48506919def7
Ryan Parsons <ryan@stackadvisors.com>
2019-03-02 10:42:14 -0700

    Updates Expense class to track past and outstanding expenses
    Expenses will be used to calculate required balances without excluding past bills.  Currently an upcoming bill will not be included in the required balance calculation if the due date is past, even though the bill has not yet been paid

--- Data/Models/Expense.cs
+++ Data/Models/Expense.cs
@@ -1,14 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
 
 namespace JPFData.Models
 {
-    public class Expense 
+    public class Expense
     {
+        public Expense()
+        {
+            Due = DateTime.Today;
+            Amount = decimal.Zero;
+            BillId = 0;
+            CreditAccount = 0;
+            IsPaid = false;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime Due { get; set; }
         public decimal Amount { get; set; }
-        public int SalaryId { get; set; }
-        [Display(Name = "Pre-Taxed?")]
-        public bool IsPreTax { get; set; }
+        public int BillId { get; set; }
+        public int CreditAccount { get; set; }
+        public bool IsPaid { get; set; }
     }
 }
