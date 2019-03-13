eb9fbdec277cd27cc3b695d5aaa0ebeec4fb5f2e
Ryan Parsons <ryan@stackadvisors.com>
2019-03-13 08:06:37 -0600

    Updates path for login

--- JPFinancial/App_Start/Startup.Auth.cs
+++ JPFinancial/App_Start/Startup.Auth.cs
@@ -25,7 +25,7 @@ namespace JPFinancial
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
+                LoginPath = new PathString("/Identity/Login"),
                 Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
