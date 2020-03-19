4de00df399813db166a2a04e8a0688098b90c2c7
Ryan Parsons <ryan.parsons@dts-global.com>
2020-03-19 15:20:33 -0600

    Updates dashboard sliders to dynamically update & moves jQuery function to new external file

---
+++ JPFinancial/Scripts/utility.js
@@ -0,0 +1,32 @@
+﻿//TODO: convert into jQuery extension (https://www.sitepoint.com/5-ways-declare-functions-jquery/)
+
+function updateMultipleRelatedSliders(changedAccount, slidersClass) {
+
+    if (Number(changedAccount.defaultValue) !== NaN && Number(changedAccount.value) !== NaN) {
+        const valueDiff = Number(changedAccount.value) - Number(changedAccount.defaultValue);
+        const sliders = $('.' + slidersClass);
+
+        sliders.each(function () {
+            const max = Number(this.max);
+
+            if (this.id === changedAccount.id) {
+                this.setAttribute('value', this.value);
+            } else {
+                if (max !== NaN) {
+                    const newMax = max - valueDiff;
+                    this.max = newMax.toString();
+                }
+            }
+        });
+
+        const disposableIncomeElement = $('#disposableIncomeValue')[0];
+        let disposableIncome = disposableIncomeElement.value.replace('$', '');
+        disposableIncome = Number(disposableIncome);
+        if (disposableIncome !== NaN) {
+            disposableIncome -= valueDiff;
+            disposableIncomeElement.value = '$' + disposableIncome.toString();
+        }
+    } else {
+        console.log("Error getting slider delta");
+    }
+};
