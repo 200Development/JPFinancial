b505bf3acdae45ddf484a6da4c4c5aef5438ab10
Ryan Parsons <ryan.parsons@dts-global.com>
2020-03-24 20:32:16 -0600

    Adds DisplayFormat data annotation to Account fields

--- Data/Models/JPFinancial/Account.cs
+++ Data/Models/JPFinancial/Account.cs
@@ -18,19 +18,19 @@ namespace JPFData.Models.JPFinancial
         [Required, StringLength(255)]
         public string Name { get; set; }
 
-        [Required, DataType(DataType.Currency)]
+        [Required, DataType(DataType.Currency), DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = true)]
         public decimal Balance { get; set; }
 
-        [Required, DataType(DataType.Currency), Display(Name = "Paycheck Contribution")]
+        [Required, DataType(DataType.Currency), DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = true), Display(Name = "Paycheck Contribution")]
         public decimal PaycheckContribution { get; set; }
 
-        [Required, DataType(DataType.Currency), Display(Name = "Suggested Paycheck Contribution")]
+        [Required, DataType(DataType.Currency), DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true), Display(Name = "Suggested Paycheck Contribution")]
         public decimal SuggestedPaycheckContribution { get; set; }
 
-        [Required, DataType(DataType.Currency), Display(Name = "Required Savings")]
+        [Required, DataType(DataType.Currency), DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true), Display(Name = "Required Savings")]
         public decimal RequiredSavings { get; set; }
 
-        [Required, DataType(DataType.Currency), Display(Name = "Surplus/Deficit")]
+        [Required, DataType(DataType.Currency), DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true), Display(Name = "Surplus/Deficit")]
         public decimal BalanceSurplus { get; set; }
 
         [Required, Display(Name = "Exclude From Surplus")]
@@ -42,7 +42,7 @@ namespace JPFData.Models.JPFinancial
         [Required, Display(Name = "Mandatory Account?")]
         public bool IsMandatory { get; set; }
 
-        [Required, Display(Name = "Balance Limit")]
+        [Required, Display(Name = "Balance Limit"), DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
         public decimal BalanceLimit { get; set; }
     }
 }
