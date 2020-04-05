245a94db9c6944a8272be3550952ce755d95746c
Ryan Parsons <ryan.parsons@dts-global.com>
2020-04-04 23:36:32 -0600

    Adds Contact and Getting Started pages w/ dropdown links in nav

---
+++ JPFinancial/Controllers/SupportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JPFinancial.Controllers
+{
+    public class SupportController : Controller
+    {
+        // GET: Support
+        public ActionResult GettingStarted()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
